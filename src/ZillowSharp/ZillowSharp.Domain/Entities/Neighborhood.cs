71b063d7fcac7bd6e235e8261079e0223508b72c
Will Swank <willswank+github@gmail.com>
2013-05-20 17:45:09 -0400

    Adding in Neighborhood Entitiy

---
+++ src/ZillowSharp/ZillowSharp.Domain/Entities/Neighborhood.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZillowSharp.Domain.Entities
+{
+    public class Neighborhood
+    {
+        public string Name { get; set; }
+    }
+}
