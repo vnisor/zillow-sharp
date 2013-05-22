71f85e0e587d45349e7ef75545a42eed233dba8d
Will Swank <willswank+github@gmail.com>
2013-05-22 16:22:24 -0400

    Adding in commands and updates to IZillowService.

---
+++ src/ZillowSharp/ZillowSharp.Domain/Commands/IZillowChangeCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ZillowSharp.Domain.Services;
+
+namespace ZillowSharp.Domain.Commands
+{
+    public interface IZillowChangeCommand
+    {
+        /// <summary>
+        /// Executes the command using the provided Zillow service.
+        /// </summary>
+        /// <param name="service"></param>
+        void Execute(IZillowService service);
+    }
+}
--- src/ZillowSharp/ZillowSharp.Domain/Factories/IHttpWebRequestFactory.cs
+++ src/ZillowSharp/ZillowSharp.Domain/Factories/IHttpWebRequestFactory.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ZillowSharp.Domain.Entities
+namespace ZillowSharp.Domain.Factories
 {
-    public interface IEntity
+    public interface IHttpWebRequestFactory
     {
 
     }
