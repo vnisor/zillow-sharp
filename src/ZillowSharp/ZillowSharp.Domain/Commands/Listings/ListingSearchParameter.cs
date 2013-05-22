056e2cb60719bed6149ddc2acae9afd120bf1db1
Will Swank <willswank+github@gmail.com>
2013-05-22 16:51:46 -0400

    Adding listing search commands

---
+++ src/ZillowSharp/ZillowSharp.Domain/Commands/Listings/ListingSearchParameter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZillowSharp.Domain.Commands.Listings
+{
+    public class ListingSearchParameter
+    {
+        /// <summary>
+        /// Initializes a new instance of the ListingSearchParameter class.
+        /// </summary>
+        public ListingSearchParameter(string fieldName, string value)
+        {
+            _fieldName = fieldName;
+            _value = value;
+        }
+
+        private readonly string _fieldName;
+        private readonly string _value;
+
+        public string FieldName
+        {
+            get
+            {
+                return _fieldName;
+            }
+        }
+        public string Value
+        {
+            get
+            {
+                return _value;
+            }
+        }
+    }
+}
---
+++ src/ZillowSharp/ZillowSharp.Domain/Commands/Listings/ListingsSearchCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZillowSharp.Domain.Entities;
+
+namespace ZillowSharp.Domain.Commands.Listings
+{
+    public class ListingsSearchCommand : IZillowFindCommand<Listing>
+    {
+
+        /// <summary>
+        /// Initializes a new instance of the ListingsSearchCommand class.
+        /// </summary>
+        public ListingsSearchCommand(IList<ListingSearchParameter> parameters)
+        {
+            
+        }
+
+        public Listing Execute(Services.IZillowService service)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
