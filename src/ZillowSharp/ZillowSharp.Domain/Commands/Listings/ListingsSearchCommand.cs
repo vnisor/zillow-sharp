c745df3a149fd451be17c400955c53eaae0fdb6f
Will Swank <willswank+github@gmail.com>
2013-05-23 21:41:56 -0400

    Adding in Infrastructure project

--- src/ZillowSharp/ZillowSharp.Domain/Commands/Listings/ListingsSearchCommand.cs
+++ src/ZillowSharp/ZillowSharp.Domain/Commands/Listings/ListingsSearchCommand.cs
@@ -10,16 +10,22 @@ namespace ZillowSharp.Domain.Commands.Listings
     public class ListingsSearchCommand : IZillowFindCommand<Listing>
     {
 
+        private readonly IList<ListingSearchParameter> _parameters;
+
         /// <summary>
         /// Initializes a new instance of the ListingsSearchCommand class.
         /// </summary>
         public ListingsSearchCommand(IList<ListingSearchParameter> parameters)
         {
-            
+            _parameters = parameters;            
         }
 
         public Listing Execute(Services.IZillowService service)
         {
+            var url = service.BuildUrl("listings.json?");
+
+            //"projects/{0}/tickets/{1}"
+
             throw new NotImplementedException();
         }
     }
